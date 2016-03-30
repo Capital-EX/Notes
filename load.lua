b87ea3b0bf529798c90d69091ea965941f54af2e
Capital-EX <xcapt98@gmail.com>
2016-03-30 15:05:45 -0400

    Worked on load system

--- load.lua
+++ load.lua
@@ -1,8 +1,21 @@
-loadNotes = function(files)
+loadNotes = function(dir,paper)
    local textBoxValues = {
         "pos",
         "wrap",
         "fontSize",
         "text",
     }
+	textFile, err = love.filesystem.newFile(dir.."text.txt",'r')
+	if err then
+		app.failedToLoad = true	
+	else
+		textData = textFile:read()
+		imgFile = love.graphics.newImage(dir.."paper.png")
+		for textBox in textData:gmatch("@textbox{(.*)}") do
+			print(textBox)
+		end
+		love.graphics.setCanvas(paper)
+			love.graphics.draw(imgFile,0,0)
+		love.graphics.setCanvas()
+	end
 end
